68010b5f267341003e668ec6a837bcc2f6ab60a8
unknown <maxmcannon@gmail.com>
2023-12-06 01:05:21 -0500

    Working on Day 5

--- Day5.py
+++ Day5.py
@@ -26,15 +26,29 @@ def tonums(nums):
 seeds = tonums(input[0].split(": ")[1].split(' '))
 print("seeds = " + str(seeds))
 
+# def buildlinemap(map):
+#     outmap = {}
+#     deststart = map[0]
+#     sourcestart = map[1]
+#     length = map[2]
+#     k = 0 
+#     for i in range(sourcestart, sourcestart + length):
+#         outmap.setdefault(i, deststart + k)
+#         k += 1
+    # return outmap
+
 def checkline(seed, map):
     deststart = map[0]
     sourcestart = map[1]
     length = map[2]
-    diff = math.fabs(seed - sourcestart)    
+    diff = math.fabs(seed - sourcestart)        # this is super close, trying a mapping solution
+    if seed < sourcestart:
+        return seed
     if diff < length:
         return(int(deststart + diff))
     else:
         return seed
+
 
 
 
@@ -46,15 +60,29 @@ def convertseed(seed):
             arr.append(tempseed)
             continue
         if not(m[0].isnumeric()):
+            print(m)
             continue
         nums = tonums(m.split(" "))
         # print(nums)
-        tempseed = checkline(tempseed, nums)        
+        getdict = buildlinemap(nums)
+        keys = getdict.keys()
+        if tempseed in keys:
+            # print(getdict[seed])                          #this function needs to be refactored to use the checkline function    
+            tempseed = getdict[tempseed]
+        else:
+            continue
+        arr.append(tempseed)
+        
     return arr
 
-print("path = " + str(convertseed(14)))
+# print("path = " + str(convertseed(14)))
       
-# for s in seeds:
-#     print(convertseed(seeds))
+
+for s in seeds:
+    print(convertseed(s))
+    if convertseed(s) < lowest:
+        lowest = convertseed(s)
+    
+print(lowest)
 
 
