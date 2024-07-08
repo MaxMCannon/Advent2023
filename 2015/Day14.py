3fab618aa38bf15a44f21802d2f02676f5422700
MaxMCannon <max@recrebox.com>
2024-07-08 18:52:08 -0400

    Day14 from 2015

--- 2015/Day14.py
+++ 2015/Day14.py
@@ -11,18 +11,42 @@ class Reindeer():
     rest_duration = 0
     pos = 0
     resting = False
+    move_counter = 0
+    rest_counter = 0
+    leader = False
+    points = 0
 
-    def updatepos(time):
-        if Reindeer.resting:
-            return None
+    def updatepos(self):
+        if self.resting:
+            if self.rest_counter == 1:
+                self.resting = False
+                self.move_counter = self.move_duration
+            self.rest_counter -= 1
         else:
-            Reindeer.pos += Reindeer.speed
+            self.pos += self.speed
+            if self.move_counter == 1:
+                self.resting = True
+                self.rest_counter = self.rest_duration
+            self.move_counter -= 1
     
-    # def printDeer():
-    #     print('Name: %s is at Pos: %s - Speed: %s for %s seconds, then rest %s seconds'
-    #           % (name, pos, speed, move_duration, rest_duration))
+    def resetcounters(self):
+        self.move_counter = self.move_duration
+        self.rest_counter = self.rest_duration
+        
+    def addpoint(self):
+        self.points += 1
+
+    def printDeer(self):
+        if self.resting == False:
+            print('Name: %s is at Pos: %s - Speed: %s for %s seconds, then rest %s seconds'
+                % (self.name, self.pos, self.speed, self.move_counter, self.rest_duration))
+        # if self.resting:
+        #     print('Name: %s is at Pos: %s - Resting for %s seconds, then moving %s for %s seconds'
+        #         % (self.name, self.pos, self.rest_counter, self.speed, self.move_duration))
 
 DeerList = []
+poslist = []
+points = []
 
 def makeDeer(l):
     deer = Reindeer()
@@ -36,18 +60,24 @@ def makeDeer(l):
 for dir in dirs:
     makeDeer(dir)
 
-time = 0 
-def printstatus():
-    print(time)
-    for d in DeerList:
-        print(str(d.name) + ' at Pos : ' + str(d.pos))
+for d in DeerList:
+    d.resetcounters()
+    poslist.append(0)
+    points.append(0)
 
-while time < 10:
-    for d in DeerList:
-        d.updatepos(time)
-    printstatus()
-    time += 1
+timer = 0
+while timer <= 2504:
+    for i in range(len(DeerList)):
+        poslist[i] = DeerList[i].pos
+        DeerList[i].printDeer()
+        DeerList[i].updatepos()
 
+    for l in range(len(poslist)):
+        if poslist[l] == max(poslist):
+            points[l] += 1
+    timer += 1
 
-
-    
+for d in DeerList:
+    print(d.name + ': ' + str(d.pos))
+print(poslist)
+print(points)
